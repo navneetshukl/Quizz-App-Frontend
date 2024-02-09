9a2551ede794ca4dc96cbbe2e93bbb513b690021
Navneet shukla <navneetshukla824@gmail.com>
2024-02-09 13:18:33 +0530

    Navbar is created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Welcome + Name of Login</div>
+  )
+}
+
+export default Home
