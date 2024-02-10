fbecd3c73575f2f9025b67ffee195971fc7cd3ae
Navneet shukla <navneetshukla824@gmail.com>
2024-02-10 18:06:35 +0530

    Quizz section is completed

--- src/components/Quizz/Quizz.jsx
+++ src/components/Quizz/Quizz.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Quizz = () => {
+  let [idx, setIdx] = useState(0);
+
+  const IncreaseIndex = () => {
+    setIdx(idx + 1);
+  };
+
+  const DecreaseIndex = () => {
+    if (idx <= 1) return;
+    setIdx(idx - 1);
+  };
+
   return (
     <div className="container">
       <div className="columns is-centered">
@@ -13,42 +24,41 @@ const Quizz = () => {
             padding: "2px auto",
             marginBottom: "2px",
             overflowY: "auto",
-            backgroundColor: "#f1faee", // to enable scrolling if content overflows
+            backgroundColor: "#f1faee",
           }}
         >
           <div className="mt-5 mx-5">
-            <h2 className="title is-4 mb-4">Question:</h2>
+            <h2 className="title is-4 mb-4">Question:{idx}</h2>
             <p className="title is-size-4 mb-4">
               This is a fixed question area.
             </p>{" "}
-            {/* Added margin bottom */}
-            <div className="mt-5"></div> {/* Added vertical spacing */}
+            <div className="mt-5"></div>
             <div className="field mb-5" style={{ marginBottom: "20px" }}>
               {" "}
               <input id="option1" type="checkbox" className="checkbox" />
               <label htmlFor="option1" className="ml-2 is-size-5">
-                Option 1
+                Option {idx}
               </label>
             </div>
             <div className="field mb-5" style={{ marginBottom: "20px" }}>
               {" "}
               <input id="option2" type="checkbox" className="checkbox" />
               <label htmlFor="option2" className="ml-2 is-size-5">
-                Option 2
+                Option {idx}
               </label>
             </div>
             <div className="field mb-5" style={{ marginBottom: "20px" }}>
               {" "}
               <input id="option3" type="checkbox" className="checkbox" />
               <label htmlFor="option3" className="ml-2 is-size-5">
-                Option 3
+                Option {idx}
               </label>
             </div>
             <div className="field mb-5" style={{ marginBottom: "20px" }}>
               {" "}
               <input id="option4" type="checkbox" className="checkbox" />
               <label htmlFor="option4" className="ml-2 is-size-5">
-                Option 4
+                Option {idx}
               </label>
             </div>
           </div>
@@ -57,9 +67,19 @@ const Quizz = () => {
       <div className="columns is-centered mt-1">
         <div className="column is-half ">
           <div className="buttons is-centered ">
-            <button className="button is-warning is-medium mx-5">Back</button>
+            <button
+              className="button is-warning is-medium mx-5"
+              onClick={DecreaseIndex}
+            >
+              Back
+            </button>
             <button className="button is-danger is-medium mx-5">Submit</button>
-            <button className="button is-primary is-medium mx-5">Next</button>
+            <button
+              className="button is-primary is-medium mx-5"
+              onClick={IncreaseIndex}
+            >
+              Next
+            </button>
           </div>
         </div>
       </div>
