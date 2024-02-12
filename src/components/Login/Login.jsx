bd57e1b0d8d1c22f2036ce3ccf34b1e2b56170ed
Navneet shukla <navneetshukla824@gmail.com>
2024-02-12 18:19:03 +0530

    Login is connected to backened

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useNavigate,Link } from "react-router-dom";
 const Login = () => {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  let navigate = useNavigate();
+
+  const formSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    console.log(await response.json());
+
+    if (response.ok) {
+      navigate("/")
+    }
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="container">
       <h1 className="title is-size-3 has-text-centered mt-5">Login</h1>
       <div className="columns is-centered mt-5">
         <div className="column is-half">
-          <form>
+          <form onSubmit={formSubmit}>
             <div className="field">
               <label className="label">Email</label>
               <div className="control">
@@ -15,6 +39,8 @@ const Login = () => {
                   type="email"
                   placeholder="Enter your email"
                   name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   required
                 />
               </div>
@@ -28,6 +54,8 @@ const Login = () => {
                   type="password"
                   placeholder="Enter your password"
                   name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
                   required
                 />
               </div>
@@ -41,6 +69,14 @@ const Login = () => {
               </div>
             </div>
           </form>
+          <div className="field " style={{"margin":"30px"}}>
+            <p>
+              Not registered?{" "}
+              <Link to="/api/register" className="button is-danger is-text is-default is-responsive is-rounded is-link is-focused ">
+                Register
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
